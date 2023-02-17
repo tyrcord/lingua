24c49c3c648cbae14b4dbae3ba66b18442da896b
Thibault Zanini <thibaultzanini@gmail.com>
2023-02-17 18:04:05 +0100

    feat: add help package

---
+++ packages/help/lib/generated/codegen_loader.g.dart
@@ -0,0 +1,78 @@
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class HelpCodegenLoader extends AssetLoader {
+  const HelpCodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ja = {
+  "help": {
+    "title": {
+      "manual": "説明"
+    }
+  }
+};
+static const Map<String,dynamic> de = {
+  "help": {
+    "title": {
+      "manual": "Anleitung"
+    }
+  }
+};
+static const Map<String,dynamic> ru = {
+  "help": {
+    "title": {
+      "manual": "Инструкции"
+    }
+  }
+};
+static const Map<String,dynamic> pt = {
+  "help": {
+    "title": {
+      "manual": "Instruções"
+    }
+  }
+};
+static const Map<String,dynamic> en = {
+  "help": {
+    "title": {
+      "manual": "Instructions"
+    }
+  }
+};
+static const Map<String,dynamic> it = {
+  "help": {
+    "title": {
+      "manual": "Istruzioni"
+    }
+  }
+};
+static const Map<String,dynamic> fr = {
+  "help": {
+    "title": {
+      "manual": "Mode d'emploi"
+    }
+  }
+};
+static const Map<String,dynamic> zh_Hans = {
+  "help": {
+    "title": {
+      "manual": "说明"
+    }
+  }
+};
+static const Map<String,dynamic> es = {
+  "help": {
+    "title": {
+      "manual": "Instrucciones"
+    }
+  }
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ja": ja, "de": de, "ru": ru, "pt": pt, "en": en, "it": it, "fr": fr, "zh_Hans": zh_Hans, "es": es};
+}
---
+++ packages/help/lib/generated/locale_keys.g.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+abstract class HelpLocaleKeys {
+  static const help_title_manual = 'help.title.manual';
+  static const help_title = 'help.title';
+  static const help = 'help';
+
+}
