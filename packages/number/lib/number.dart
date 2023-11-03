4e19cda3c2f3fd0ecfac3cca15b318ea909d8878
Thibault Zanini <thibaultzanini@gmail.com>
2023-11-03 10:06:17 +0100

    feat: add toOrdinal helper

--- packages/number/lib/number.dart
+++ packages/number/lib/number.dart
@@ -1,1 +1,30 @@
 library lingua_number;
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:lingua_core/lingua_core.dart';
+import './generated/locale_keys.g.dart';
+
+String toOrdinal(int number, {String gender = LinguaLocalizationGender.male}) {
+  final int tens = (number / 10).floor() % 10;
+  late String key;
+
+  if (tens == 1) {
+    key = NumberLocaleKeys.number_ordinal_short_others;
+  } else {
+    final int ones = number % 10;
+
+    switch (ones) {
+      case 1:
+        key = NumberLocaleKeys.number_ordinal_short_first;
+      case 2:
+        key = NumberLocaleKeys.number_ordinal_short_second;
+      case 3:
+        key = NumberLocaleKeys.number_ordinal_short_third;
+      default:
+        key = NumberLocaleKeys.number_ordinal_short_others;
+        break;
+    }
+  }
+
+  return key.tr(namedArgs: {"num": number.toString()}, gender: gender);
+}
